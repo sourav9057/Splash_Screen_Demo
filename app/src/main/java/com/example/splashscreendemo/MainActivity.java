43eeccab330dc81389b605bb4ec412ecaad19a73
sourav9057 <souravkumar9057@gmail.com>
2024-03-17 23:56:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashscreendemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.splashscreendemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIMER = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
